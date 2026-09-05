3ffd677d9311ce34135ff13b5a9a6df7e935e719
Ander Vilariño <andervlrn@gmail.com>
2026-09-05 22:59:14 +0200

    New: BookingProcess section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import BookingProcess from "./components/BookingProcess"
 import Dishes from "./components/Dishes"
 import Features from "./components/Features"
 import Hero from "./components/Hero"
@@ -14,6 +15,7 @@ const App = () => {
       <Stats />
       <Dishes />
       <Features />
+      <BookingProcess />
     </>
   )
 }
