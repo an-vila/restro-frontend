d9873c473f3bffb53afd9ecb71c5ff992341c379
Ander Vilariño <andervlrn@gmail.com>
2026-09-05 20:45:21 +0200

    New: Added Dishes section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Dishes from "./components/Dishes"
 import Hero from "./components/Hero"
 import Navbar from  "./components/Navbar"
 import Stats from "./components/Stats"
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Stats />
+      <Dishes />
     </>
   )
 }
